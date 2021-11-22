fd9dbc0c5c03c784ab03f8efdb04262471e9cdfd
Frederik Bonte <fbonte@rocvantwente.nl>
2021-11-22 12:00:18 +0100

    Created an initial Player class... @TODO: implement room navigation and treasure handling.

---
+++ DoolhofProject2021/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoolhofProject2021
+{
+    public class Player
+    {
+        // In which room is the player?
+        private Room room;
+        // Name of the player.
+        private string name;
+        // How much treasure does the player have?
+        private int treasure;
+
+        public Player(string name, Room room)
+        {
+            this.name = name;
+            this.room = room;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public Room getRoom()
+        {
+            return room;
+        }
+    }
+}
